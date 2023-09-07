999b8a6ab6369d44621f03422692d8eb5c75f880
Jacob Di Lorenzo <jadilorenzo@icloud.com>
2023-09-07 16:39:53 -0400

    bring over css

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
-  has_many :game_users
+  has_many :game_users, dependent: :destroy
   has_many :users, through: :game_users
 
   validates :player_count, presence: true, numericality: { greater_than: 1 }
